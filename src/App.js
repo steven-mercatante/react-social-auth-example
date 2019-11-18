fa17c6de6a321faaec083c2c6dd0aac17b410bfc
Steven Mercatante <steven.mercatante@gmail.com>
2019-11-18 15:35:10 -0500

    implement log out

--- src/App.js
+++ src/App.js
@@ -1,53 +1,71 @@
-import React, { useEffect, useReducer, useRef } from "react";
-import request from "axios";
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 import "./App.css";
 
-function getCookie(name) {
-  let cookie = {};
-  document.cookie.split(";").forEach(function(el) {
-    let [k, v] = el.split("=");
-    cookie[k.trim()] = v;
-  });
-  return cookie[name];
+function getHeaders() {
+  const headers = {
+    Authorization: `Token ${Cookies.get("auth_token")}`
+  };
+  return headers;
 }
-
-request
-  .get("http://127.0.0.1:8000/clients/", {
-    headers: {
-      Authorization: `Token ${getCookie("auth_token")}`
-    }
-  })
-  .then(resp => {
-    console.log("resp", resp.data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
 
 function authReducer(state, event) {
   switch (event.type) {
     case "AUTH":
       return { ...state, status: "authenticating" };
     case "SUCCESS":
-      return { ...state, status: "success" };
+      return { ...state, status: "authenticated" };
+    case "LOGOUT":
+      return { ...state, status: "idle" };
     default:
       return state;
   }
 }
 
 const initialState = {
-  status: "idle"
+  status: Cookies.get("auth_token") ? "authenticated" : "idle"
 };
+
+function Clients() {
+  const [clients, setClients] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/clients/", {
+        headers: getHeaders()
+      })
+      .then(resp => {
+        setClients(resp.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>Clients</h2>
+      <ul>
+        {clients.map(client => (
+          <li key={client.id}>
+            <span>{client.name}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
   const [state, dispatch] = useReducer(authReducer, initialState);
   const popupRef = useRef(null);
 
   useEffect(() => {
+    // console.log("useEffect state:", state);
     if (state.status === "authenticating") {
       openPopup();
     }
-  }, [state.status]);
+  }, [state, state.status]);
 
   function openPopup() {
     const width = 600;
@@ -68,14 +86,44 @@ function App() {
       // TODO: update origin URL once everything is moved to production
       if (message.origin !== "http://127.0.0.1:8000") return;
       console.log("received message from backend", message.data);
+      if (message.data === "AUTH_SUCCESS") {
+        dispatch({ type: "SUCCESS" });
+      }
     });
+  }
+
+  function handleLogout() {
+    dispatch({ type: "LOGOUT" });
+    Cookies.remove("auth_token");
+
+    axios
+      .post(
+        "http://127.0.0.1:8000/users/logout",
+        {},
+        {
+          headers: getHeaders()
+        }
+      )
+      .then(resp => {
+        console.log("log out resp:", resp);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   return (
     <div className="App">
-      <button onClick={() => dispatch({ type: "AUTH" })}>
-        Login via GitHub
-      </button>
+      {state.status === "authenticated" ? (
+        <>
+          <button onClick={handleLogout}>log out</button>
+          <Clients />
+        </>
+      ) : (
+        <button onClick={() => dispatch({ type: "AUTH" })}>
+          Login via GitHub
+        </button>
+      )}
     </div>
   );
 }
